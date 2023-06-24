5b3d41bbeaff6d5a1ba9229aef01dca21a98ce5a
Koi <kkoiwa.1720+git@gmail.com>
2023-06-24 12:04:42 +0900

    Notion DB 読み込み機能追加

--- overtime-checker/functions.py
+++ overtime-checker/functions.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Functions:
     def __init__(self):
         pass
@@ -8,3 +11,14 @@ class Functions:
             url =  f.readline().rstrip('\n')
         l = url.split('/')
         return l[4].split('?')[0]
+
+    @classmethod
+    def sum_of_timelist(self, timelist):
+        sum = datetime.timedelta(0)
+        for time in timelist:
+            hours, minutes = map(int, time.split(":"))
+            time = datetime.timedelta(hours=hours, minutes=minutes)
+            sum = sum + time
+
+        total_seconds = sum.total_seconds()
+        return str(int(total_seconds // 3600)) + ':' + '{:0>2}'.format(str(int(total_seconds % 3600 // 60)))
--- overtime-checker/notion_operator.py
+++ overtime-checker/notion_operator.py
@@ -1,3 +1,5 @@
+import sys
+
 import requests
 
 
@@ -19,5 +21,31 @@ class NotionOperator:
         requests.post(request_url, headers=headers, json=json)
 
     @classmethod
-    def read_records(self, notion_token, db_url, month):
-        pass
+    def read_records(self, notion_token, db_id, year_month):
+        req_url = 'https://api.notion.com/v1/databases/' + db_id + '/query'
+        headers = {'Authorization': 'Bearer ' + notion_token,
+                   'Content-Type': 'application/json; charset=UTF-8',
+                   'Notion-Version': '2022-06-28'}
+
+        try:
+            response = requests.post(url=req_url, headers=headers)
+            response.raise_for_status()
+        except Exception:
+            print('Error: DB URL or token is invalid.')
+            sys.exit(1)
+        json_data = response.json()
+        results = json_data.get('results')
+
+        try:
+            overtime_of_month = {}
+            for result in results:
+                date = result['properties']['Date']['title'][0]['text']['content']
+                time = result['properties']['Time']['rich_text'][0]['text']['content']
+                if date[:-3] == year_month:
+                    overtime_of_month[date] = time
+            overtime_of_month = dict(sorted(overtime_of_month.items()))
+        except Exception:
+            print('Error: Name or type of column is wrong.')
+            sys.exit(1)
+
+        return overtime_of_month
