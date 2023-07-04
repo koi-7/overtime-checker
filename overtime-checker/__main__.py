144b40dcad31743e0e245a70881ec557baf55d66
Koi <kkoiwa.1720+git@gmail.com>
2023-07-04 22:27:49 +0900

    config ファイル

--- overtime-checker/__main__.py
+++ overtime-checker/__main__.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 
+import configparser
 import datetime
 import logging
 import os
@@ -14,13 +15,10 @@ from .slack_notifier import *
 
 
 MAIN_DIR = os.path.dirname(__file__)
+CONFIG_PATH = os.path.join(MAIN_DIR, '../config/config.ini')
+CREDENTIALS_PATH = os.path.join(MAIN_DIR, '../config/credentials.json')
+GOOGLE_TOKEN_PATH = os.path.join(MAIN_DIR, '../config/google_token.json')
 LOG_PATH = os.path.join(MAIN_DIR, '../logs/overtime-checker.log')
-DATA_DIR = os.path.join(MAIN_DIR, '../data/')
-CREDENTIALS_PATH = os.path.join(DATA_DIR, 'credentials.json')
-GOOGLE_TOKEN_PATH = os.path.join(DATA_DIR, 'google_token.json')
-NOTION_TOKEN_PATH = os.path.join(DATA_DIR, 'notion_token')
-DB_URL_PATH = os.path.join(DATA_DIR, 'database_url')
-SLACK_TOKEN_PATH = os.path.join(DATA_DIR, 'slack_token')
 
 
 logger = getLogger(__name__)
@@ -32,6 +30,9 @@ logger.addHandler(handler)
 
 
 def main():
+    config_ini = configparser.ConfigParser()
+    config_ini.read(CONFIG_PATH, encoding='utf-8')
+
     # Google 認証
     try:
         creds = CalendarReader.get_credentials(CREDENTIALS_PATH, GOOGLE_TOKEN_PATH)
@@ -41,27 +42,26 @@ def main():
 
     today = str(datetime.datetime.today().date())  # 今日の日付（yyyy-MM-dd）
     overtime = CalendarReader.get_overtime(creds, today)  # 残業時間（hh:mm）
+    today = today.replace('-', '/')  ## yyyy-MM-dd -> yyyy/MM/dd
 
     # Notion トークン、DB ID 取得
-    with open(NOTION_TOKEN_PATH, 'r') as f:
-        notion_token = f.readline().rstrip('\n')
-    database_id = Functions.url_to_dbid(DB_URL_PATH)
-
-    today = today.replace('-', '/')  ## yyyy-MM-dd -> yyyy/MM/dd
+    notion_token = config_ini['Notion']['token']
+    notion_database_url = config_ini['Notion']['database_url']
+    l = notion_database_url.split('/')
+    notion_database_id = l[4].split('?')[0]
 
     # 残業時間があれば DB に記録
     if overtime:
-        NotionOperator.insert_record(notion_token, database_id, today, overtime)
+        NotionOperator.insert_record(notion_token, notion_database_id, today, overtime)
 
     # 翌日の日付 yyyy/MM/dd の dd が '01' なら
     if str(datetime.datetime.today().date() + datetime.timedelta(1))[-2:] == '01':
-        overtime_dict = NotionOperator.read_records(notion_token, database_id, today[:-3])  # 残業日付・残業時間の辞書取得
+        overtime_dict = NotionOperator.read_records(notion_token, notion_database_id, today[:-3])  # 残業日付・残業時間の辞書取得
 
         sum = Functions.sum_of_timelist(list(overtime_dict.values()))  # ひと月の合計残業時間取得
 
         # Slack トークン取得
-        with open(SLACK_TOKEN_PATH, 'r') as f:
-            slack_token = f.readline().rstrip('\n')
+        slack_token = config_ini['Slack']['token']
 
         # Slack で通知
         SlackNotifier.send_notify(slack_token, overtime_dict, sum)
